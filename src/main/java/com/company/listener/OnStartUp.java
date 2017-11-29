16eba4bb5e75c13a2e245a44c5b467fb42bb9d38
Maxym Borodenko <maksimborodenko@mail.ru>
2017-11-29 11:41:50 +0200

    PageRequest constructor is deprecated

--- src/main/java/com/company/listener/OnStartUp.java
+++ src/main/java/com/company/listener/OnStartUp.java
@@ -38,14 +38,14 @@ public class OnStartUp implements InitializingBean {
 
         LOG.info("start up...");
 
-        //printElasticSearchInfo();
+        printElasticSearchInfo();
 
         bookService.save(new Book("1001", "Elasticsearch Basics", "Rambabu Posa", "23-FEB-2017"));
         bookService.save(new Book("1002", "Apache Lucene Basics", "Rambabu Posa", "13-MAR-2017"));
         bookService.save(new Book("1003", "Apache Solr Basics", "Rambabu Posa", "21-MAR-2017"));
 
         //fuzzey search
-        Page<Book> books = bookService.findByAuthor("Rambabu", new PageRequest(0, 10));
+        Page<Book> books = bookService.findByAuthor("Rambabu", PageRequest.of(0, 10));
 
         //List<Book> books = bookService.findByTitle("Elasticsearch Basics");
 
